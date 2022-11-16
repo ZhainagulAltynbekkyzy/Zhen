f60b889848ed9ddd80a662ff92db0c4699fc6547
renkoike-o83aqr <o83aqr@inf.elte.hu>
2022-11-16 16:59:04 +0100

    recreate the structure of the repo

---
+++ src/main/java/com/example/zhen/ZhenApplication.java
@@ -0,0 +1,13 @@
+package com.example.zhen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zhen/ZhenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zhen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
